752bfa33a45dd3f61035d3cbf3684529bd3871d5
Mayen007 <alonerpunk@gmail.com>
2025-05-11 18:31:21 +0300

    Remove unnecessary blank line in app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from models import Post, Comment, Event
 from datetime import datetime
 
 
-
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
